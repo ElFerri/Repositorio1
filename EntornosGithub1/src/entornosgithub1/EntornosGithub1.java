7cddef6203fda577e521061ddac7235dab35e0dd
fersan <fersan@ubuntu>
2021-11-23 15:52:59 +0100

    Se modifico a Mitcode

--- EntornosGithub1/src/entornosgithub1/EntornosGithub1.java
+++ EntornosGithub1/src/entornosgithub1/EntornosGithub1.java
@@ -15,6 +15,7 @@ public class EntornosGithub1 {
      */
     public static void main(String[] args) {
         System.out.println("Bon dia ");
+        System.out.println("Buena Tarde");
     }
     
 }
